fab5c58b97c263455ecc636d3913600eacb35992
SIGMA - Isaac Nascimento Pereira <isaac.pereira@saude.gov.br>
2022-01-20 21:02:56 -0300

    first commit

---
+++ src/main/java/org/com/iurdapp/IurdappApplication.java
@@ -0,0 +1,13 @@
+package org.com.iurdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IurdappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IurdappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/com/iurdapp/IurdappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.iurdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IurdappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
